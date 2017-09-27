748c364afb825e95c4e1cd66cfa6ac884958c44f
Nathan Jent <n.jent@edaptive.com>
2017-09-27 17:05:12 -0400

    Update to move send_response into response.rs

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -6,11 +6,13 @@ extern crate query_parse;
 
 mod errors;
 mod request;
+mod response;
 mod static_content;
 
 pub use errors::Error;
 pub use static_content::get_page;
-pub use request::{build_request_from_env, send_response};
+pub use request::{build_request_from_env};
+pub use response::{send_response};
 pub use query_parse::{Query, QueryValue};
 
 pub type Result<T> = std::result::Result<T, errors::Error>;
